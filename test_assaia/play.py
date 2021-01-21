9c52caba65dc0de4e64c1a878345d8c4996f5b84
Svetlana K aka myFelis <nan.tyit@gmail.com>
2021-01-21 18:22:32 +0300

    fixed gravity and units

--- test_assaia/play.py
+++ test_assaia/play.py
@@ -5,6 +5,10 @@ WIDTH = 7
 NUMBERS_IN_LINE = 4
 EMPTY_MARK = 0
 
+DIGITS = {"R": 1, "B": 2, "G": 3}
+MARKS = {EMPTY_MARK: "-", 1: "R", 2: "B", 3: "G"}
+players_samples = [("Rich", "R"), ("Big", "B"), ("Great", "G")]
+
 
 class SceneError(Exception):
     """
@@ -13,13 +17,24 @@ class SceneError(Exception):
     pass
 
 
+class Player(object):
+
+    def __init__(self, name, mark):
+        self.name = name
+        self.mark = mark
+        self.digit = DIGITS.get(mark)
+
+    def __str__(self):
+        return self.name
+
+
 class Scene(object):
 
     def __init__(self, **kw):
         self.height = kw.get("height") or HEIGHT
         self.width = kw.get("width") or WIDTH
         self.number_in_line = kw.get("nline") or NUMBERS_IN_LINE
-        self._players = {'Yellow': "Y", "Green": "G"}
+        self._players = [Player(*players_sample) for players_sample in players_samples]
         self.marks = self.create_scene()
 
     @property
@@ -40,36 +55,53 @@ class Scene(object):
             marks.append([EMPTY_MARK for j in range(0, self.height)])
         return marks
 
-    def check_winner(self, cell, name):
+    def match_nearest(self, digit, cell, next_p):
+        d = self.marks
+        template = str(digit) * self.number_in_line
+        vertical = ''.join(str(mark) for mark in d[cell])
+        match = template in vertical
+        if not match:
+            horizontal = ''.join(str(d[i][next_p]) for i in range(0, self.width))
+            match = template in horizontal
+        if not match:
+            # TODO: here should be smart counter for all these digits from the self.marks
+            diagonal = ""
+        return match
+
+    def check_winner(self, name, cell, next_p):
         """
         horizontal, vertical or diagonal checking for NUMBERS_IN_LINE similar marks in line
         :return: result
         """
-        result = True if len([name for mark in self.marks[cell] if name == mark]) > self.number_in_line else False
-        # TODO: check also vertical and diagonal matches
-        if not result:
-            pass
-        return result
+        digit = name.digit
+        return self.match_nearest(digit, cell, next_p)
 
-    def set_turn(self, turn, mark):
+    def set_turn(self, turn, player):
         """
         setting the player turn
         :return: result
         """
+        # gravitation's stroong
         current_length = sum([1 for cell in self.marks[turn] if cell != EMPTY_MARK])
-        next_position = self.width - current_length - 1
+        next_position = self.height - current_length - 1
         if next_position < 0:
             raise SceneError('Cannot add the turn')
-        self.marks[turn][self.height - current_length - 1] = mark
+        next_p = self.width - current_length - 1
+        self.marks[turn][next_p] = player.digit
+        return next_p
 
     def show_scene(self):
         """
         Show current state of the scene
         """
-        for i in range(len(self.marks[0])):
-            for j in range(len(self.marks)):
-                print(self.marks[i][j], end=',')
+        for i in range(len(self.marks)):
+            for j in range(len(self.marks[0])):
+                print(MARKS.get(self.marks[j][i]), end=',')
             print('\n')
+
+
+class GameOver(Exception):
+    pass
 
 
 if __name__ == '__main__':
@@ -93,14 +125,15 @@ if __name__ == '__main__':
                     continue
                 try:
                     turn = int(turn)
-                    mark = name[0]
-                    ceil = scene.set_turn(turn, mark)
-                    win = scene.check_winner(turn, mark)
+                    cell = scene.set_turn(turn, name)
+                    win = scene.check_winner(name, turn, cell)
                 except SceneError:
                     print('Please, try again (wrong turn)')
                     continue
                 if win:
+                    scene.show_scene()
                     print(f'Player {name} won')
+                    raise GameOver("Thank you and Good Luck")
                 else:
                     break
             scene.show_scene()
--- test_assaia/test.py
+++ test_assaia/test.py
@@ -1,16 +1,20 @@
 from unittest import TestCase
-from play import Scene
+from play import Scene, Player
 
 
 class SceneTestCase(TestCase):
+
     def test_check_winner_false(self):
         scene = Scene()
+        player = Player('test', "R")
+
         for i in range(0, 3):
-            scene.set_turn(0, "G")
-        self.assertEqual(False, scene.check_winner(0, "Y"))
+            scene.set_turn(0, player)
+        self.assertEqual(False, scene.check_winner(player, 1, 1))
 
     def test_check_winner_true(self):
         scene = Scene()
-        for i in range(0, 4):
-            scene.set_turn(i, "Y")
-        self.assertEqual(True, scene.check_winner(1, "Y"))
+        player = Player('test', "G")
+        for i in [1, 1, 1, 1]:
+            scene.set_turn(1, player)
+        self.assertEqual(True, scene.check_winner(player, 1, 1))
